3fda95e8171ba10f49c9f926e2f67a0395487896
5tarlight <developerstarlight@gmail.com>
2019-02-23 17:08:21 +0900

    initialize router

--- main.js
+++ main.js
@@ -2,14 +2,19 @@ const express = require('express')
 const http = require('http')
 const path = require('path')
 const parser = require('body-parser')
+const loader = require('./lib/public/routes/loader')
 
 const app = express()
 
 app.set('port', 80)
+app.set('views', path.join(__dirname, '/lib/public/routes/views'))
+app.set('view engine', 'pug')
 
 app.use('/', express.static(path.join(__dirname, '/lib/public')))
 app.use(parser.urlencoded({ extended: true }))
 app.use(parser.json())
+
+loader.init(app, express.Router())
 
 http.createServer(app).listen(app.get('port'), () => {
   console.log('Serer On!')

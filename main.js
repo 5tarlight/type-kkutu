50fdc66f51848a75bf3e8afcba7a3bc276627140
5tarlight <developerstarlight@gmail.com>
2019-02-23 16:46:12 +0900

    static path

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const http = require('http')
+const path = require('path')
 
 const app = express()
 
 app.set('port', 80)
+
+app.use('/', express.static(path.join(__dirname, '/lib/public')))
 
 http.createServer(app).listen(app.get('port'), () => {
   console.log('Serer On!')

1b1777c142800b7288ecfa00da3f68c59261f364
5tarlight <developerstarlight@gmail.com>
2019-02-23 16:50:03 +0900

    POST supported

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const http = require('http')
 const path = require('path')
+const parser = require('body-parser')
 
 const app = express()
 
 app.set('port', 80)
 
 app.use('/', express.static(path.join(__dirname, '/lib/public')))
+app.use(parser.urlencoded({ extended: true }))
+app.use(parser.json())
 
 http.createServer(app).listen(app.get('port'), () => {
   console.log('Serer On!')

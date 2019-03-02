fb1178b9bcd2e5a30785e37b9f224f69cbfddab9
5tarlight <developerstarlight@gmail.com>
2019-03-03 01:38:17 +0900

    topic scheme

--- db.sql
+++ db.sql
@@ -1,9 +1,18 @@
-CREATE TABLE `user` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`username` VARCHAR(20) NOT NULL,
-	`password` VARCHAR(45) NOT NULL,
-	PRIMARY KEY (`id`)
-)
-COMMENT='user list'
-ENGINE=InnoDB
-;
+CREATE DATABASE IF NOT EXISTS `zero`
+USE `zero`;
+
+CREATE TABLE IF NOT EXISTS `topic` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(30) NOT NULL DEFAULT '<제목이 지정되지 않음>',
+  `description` varchar(50) DEFAULT '설명 없음',
+  `article` longtext NOT NULL,
+  `authorid` bigint(20) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='user list';

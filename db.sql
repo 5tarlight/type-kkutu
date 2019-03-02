01e0eb5a5562041783f531f6d01528c8897fe6b7
5tarlight <developerstarlight@gmail.com>
2019-03-03 02:03:23 +0900

    topic detection

--- db.sql
+++ db.sql
@@ -1,18 +1,42 @@
-CREATE DATABASE IF NOT EXISTS `zero`
+-- --------------------------------------------------------
+-- 호스트:                          localhost
+-- 서버 버전:                        10.3.10-MariaDB - mariadb.org binary distribution
+-- 서버 OS:                        Win64
+-- HeidiSQL 버전:                  9.4.0.5125
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- zero 데이터베이스 구조 내보내기
+CREATE DATABASE IF NOT EXISTS `zero` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `zero`;
 
+-- 테이블 zero.topic 구조 내보내기
 CREATE TABLE IF NOT EXISTS `topic` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(30) NOT NULL DEFAULT '<제목이 지정되지 않음>',
   `description` varchar(50) DEFAULT '설명 없음',
   `article` longtext NOT NULL,
   `authorid` bigint(20) unsigned NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `title` (`title`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
+-- 내보낼 데이터가 선택되어 있지 않습니다.
+-- 테이블 zero.user 구조 내보내기
 CREATE TABLE IF NOT EXISTS `user` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(20) NOT NULL,
   `password` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='user list';
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='user list';
+
+-- 내보낼 데이터가 선택되어 있지 않습니다.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
